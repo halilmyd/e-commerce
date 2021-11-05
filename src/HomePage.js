2fedce5fb69a2ceaff9e5f2f4f1bfda5e276183f
İbrahim <ibo.myd@hotmail.com>
2021-11-05 05:25:09 +0300

    Functions have been written for the loginpage

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,10 +1,37 @@
 import React from 'react'
 
- const HomePage = () => {
+
+
+const HomePage = () => {
+
+    
     return (
-        <div>
-            <h1>Burası Anasayfa</h1>
+        <div className="HomePage">
+            <div>
+                <h3 className="header-categories">Kategoriler</h3>
+                <div className="categories">
+                            <ul>
+                                <li><a>Gıda</a>
+                                     <ul>
+                                        <li><a href="#">Peynir</a></li>
+                                    </ul>
+                                </li>
+                                <li><a>Giyim</a>
+                                     <ul>
+                                        <li><a href="#">Kadın Giyim</a></li>
+                                    </ul>
+                                </li>
+                                <li><a>Teknoloji</a>
+                                     <ul>
+                                        <li><a href="#">Bilgisayar</a></li>
+                                    </ul>
+                                </li>
+
+                            </ul>
+                        </div>
+            </div>
         </div>
     )
 }
 export default HomePage;
+
--- src/Nav.js
+++ src/Nav.js
@@ -11,12 +11,36 @@ function Nav() {
 
     return (
         <div className="Nav">
+             <ul className="description">       
+                 <li>Temizlikten gıdaya, kahveden oyuncağa 1000'in üzerinde ürünü avantajlı fiyatlarla alın!</li>            
+                 <li><a href="#">İletişim</a></li> 
+                 <li><a href="#">Kampanyalar</a></li>
+                             
+             </ul>
             <nav>
-                <ul className="nav-Links">
+                <ul className="nav-Links">      
                     <Link to="/">
-                        <h2><li className="hknd">Anasayfa</li></h2>
+                        <h2 className="home-page"><img className="logo" src="https://imgyukle.com/f/2021/11/05/kOZKhA.jpg" /></h2>
                     </Link>
-                </ul>  
+                    <div className="ui search">
+                        <div className="ui icon input">
+                            <input className="ınput" type="text" placeholder="Ürün arayın.." />
+                            <i className="search icon"></i>
+                        </div>
+                        <div className="results"></div>
+                    </div>
+                    <div>
+                        <div class="ui labeled button" tabindex="0">
+                            <div class="ui red button">
+                                <i class="heart icon"></i>Favoriler
+                            </div>
+                        </div>
+                        <button className="ui right labeled icon button" onClick={() => cıkısYap()}>
+                            <i className="right arrow icon"></i>Çıkıs</button>
+                    </div>
+                </ul>
+
+
             </nav>
         </div>
     )
